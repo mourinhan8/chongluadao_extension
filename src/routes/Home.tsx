9ae7fa375dc589214666f0bb2fae9259a1843275
ngocnhan <sbtc.walkingdead@gmail.com>
2023-04-19 14:39:53 +0700

    fix

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react"
-import { useHistory } from "react-router-dom"
 import { getCurrentTabUrl } from "../chrome/utils"
 import "../assets/styles/tailwind.css"
 import logo from "../assets/img/tcu.jpeg"
@@ -8,14 +7,23 @@ import axios from "axios"
 export const Home = () => {
   const [url, setUrl] = useState<string>("")
   const [warning, setWarning] = useState<string>("")
-
-  let { push } = useHistory()
+  const [notFound, setNotFound] = useState(0)
 
   const fetchAPI = async (link: any) => {
-    const { data } = await axios.post("http://localhost:8000/url_check", {
+    const { data } = await axios.post("http://localhost:4001/url/detect-url", {
       url: link,
     })
-    setWarning(data.msg)
+    if (data.data.response.resultDetection === "safe") {
+      setWarning("Đây là đường dẫn an toàn")
+    }
+    if (data.data.response.resultDetection === "malicious") {
+      setWarning("Đây là đường dẫn độc hại")
+    }
+    if (data.data.response.notFound) {
+      setNotFound(1)
+    } else {
+      setNotFound(0)
+    }
   }
 
   /**
@@ -23,8 +31,13 @@ export const Home = () => {
    */
   useEffect(() => {
     getCurrentTabUrl((url) => {
-      setUrl(url || "undefined")
-      fetchAPI(url)
+      const url_split = url?.split("/")
+      let urlParam
+      if (url_split) {
+        urlParam = url_split[2]
+      }
+      setUrl(urlParam || "undefine")
+      fetchAPI(urlParam)
     })
   }, [])
 
@@ -40,15 +53,20 @@ export const Home = () => {
       <div className="mx-auto max-w-screen-xl px-4 pt-4">
         <div className="mx-auto max-w-lg text-center">
           <h1 className="text-lg font-bold sm:text-3lg">
-            Tiện ích phát hiện lừa đảo
+            Nhận diện đường dẫn độc hại
           </h1>
-          <p className=" text-gray-500 text-[12px]">
-            Made by Telecommunication university
-          </p>
+          <p className=" text-gray-500 text-[12px]">Made by TCU</p>
         </div>
       </div>
-      <p>{url.split("/")[2]}</p>
-      <p>{warning}</p>
+      <div className="pt-4">
+        {/* <p>hello</p> */}
+        <p>{url}</p>
+        {notFound === 1 && <p>Không có dữ liệu về đường dẫn từ whois</p>}
+      </div>
+      <div className="pt-4">
+        {/* <p className="font-bold text-[20px]">Hello</p> */}
+        <p className="font-bold text-[20px]">{warning}</p>
+      </div>
     </div>
   )
 }
